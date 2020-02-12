37e8545d5540151fc57ccf43f4b85533cf3e62d7
James Rogers <jarogers095@gmail.com>
2020-02-12 03:41:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,3 @@
 class Song
   attr_accessor(:title, :artist, :genre)
-  
-  #def initialize(song_title, song_artist, song_genre)
-    #@title = song_title
-    #@artist = song_artist
-    #@genre = song_genre
-  #end
-  
 end
